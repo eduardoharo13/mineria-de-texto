2e8fa7c4e43b5d9818335317811dc36f55559027
eduardoharo13 <eduardo.haro@pucp.edu.pe>
2021-06-11 14:54:52 -0500

    segundo commit

--- analisis.R
+++ analisis.R
@@ -10,17 +10,17 @@ charlot15<-read.csv("D:/Maestria_Estadistica_PUCP/III-ciclo/temas en estadistica
 charlot16<-read.csv("D:/Maestria_Estadistica_PUCP/III-ciclo/temas en estadistica contemporanea/charlotesville/aug16_sample/aug16_sample.csv",header = T, sep=",")
 charlot17<-read.csv("D:/Maestria_Estadistica_PUCP/III-ciclo/temas en estadistica contemporanea/charlotesville/aug17_sample/aug17_sample.csv",header = T, sep=",")
 charlot18<-read.csv("D:/Maestria_Estadistica_PUCP/III-ciclo/temas en estadistica contemporanea/charlotesville/aug18_sample/aug18_sample.csv",header = T, sep=",")
+#tweet_count_time_series<-read.csv("D:/Maestria_Estadistica_PUCP/III-ciclo/temas en estadistica contemporanea/charlotesville/tweet_count_time_series.csv/tweet_count_time_series.csv",header = T, sep=",")
 
+#Realizamos un append entre las 2 bases de datos
+charlot<-charlot15
 
+# Aislar el texto con los tweets
+charlot_tweets <- charlot$full_text
 
-
-# Aislar el texto con los tweets: coffee_tweets
-charlot_tweets <- charlot15$full_text
 
 # Creaci?n de un corpus (colecci?n de documentos)                       
 #-------------------------------------------------
-
-
 # Crear un vector de tipo source 
 # (interpretar cada elemento del vector como un documento)
 charlot_source <- VectorSource(charlot_tweets)
@@ -31,39 +31,37 @@ charlot_corpus <- VCorpus(charlot_source)
 # Mostrar el objeto
 charlot_corpus
 
-# Mostrar el 15avo tweet en coffee_corpus
-charlot_corpus[[15]][1]
-content(charlot_corpus[[15]])
-
-# Mostrar los metadatos del 15avo tweet en coffee_corpus
-charlot_corpus[[15]][2]
-meta(charlot_corpus[[15]])
+# Mostrar el 1er tweet del corpus
+charlot_corpus[[1]][1]
+content(charlot_corpus[[1]])
 
 
-
-sw<-stopwords(language = "en", source = "snowball", simplify = TRUE)
-toSpace = content_transformer( function(x, pattern) gsub(pattern," ",x) )
-removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
 
 # Limpieza y Preprocesamiento                      
 #-------------------------------------------------
+#stopwords
+sw<-stopwords::stopwords("en", source = "snowball")
+#funcion para remover las urls
+removeURL <- function(x) gsub("http[^[:space:]]*", "", x)
+
 clean_corpus <- function(corpus){
   corpus <- tm_map(corpus, removePunctuation)
   corpus <- tm_map(corpus, content_transformer(tolower))
   corpus <- tm_map(corpus, removeWords, sw)
   corpus <- tm_map(corpus, stripWhitespace)
-  corpus<- tm_map(corpus,removeURL)
+  corpus<- tm_map(corpus,content_transformer(removeURL))
   return(corpus)
 }
 
 # Aplicar la funci?n personalizada 
 clean_corp <-clean_corpus(charlot_corpus)
 
+
 # Mostrar los tweets limpios
-clean_corp[[33]][1]
+clean_corp[[1]][1]
 
 # Mostrar el texto original
-charlot15$full_text[33]
+charlot15$full_text[1]
 
 
 # Crear un Document Term Matrix (DTM)                    
@@ -72,18 +70,16 @@ charlot15$full_text[33]
 charlot_dtm <- DocumentTermMatrix(clean_corp)
 
 # Mostrar el dtm
-coffee_dtm
+charlot_dtm
 
 # Convertir dtm a matriz
-coffee_m <- as.matrix(coffee_dtm)
+charlot_m <- as.matrix(charlot_dtm)
 
 # Mostrar las dimensiones
-dim(coffee_m)
+dim(charlot_m)
 
 # crea matriz de terminos
 charlot_tdm<- TermDocumentMatrix(clean_corp)
-m_discursos
+charlot_tdm
 
-#Matriz inversa dtm.tfidf_discursos 
-dtm.tfidf_discursos <- DocumentTermMatrix(discursos, control=list(weighting=weightTfIdf))
-dtm.tfidf_discursos
+
